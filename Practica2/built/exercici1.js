1d673602ebffb0b08fefc6518bf66fdb0907fb2d
SGomez10 <sgomez@elpuig.xeill.net>
2024-10-24 19:49:08 +0200

    Ejercicio 3 terminado

--- Practica2/built/exercici1.js
+++ Practica2/built/exercici1.js
@@ -8,13 +8,24 @@ do {
         break;
     }
     else {
-        var productName = prompt("Introduce el nombre del producto");
-        var productPrice = Number(prompt("Introduce el precio del producto"));
-        var productQuant = Number(prompt("Introduce la cantidad del producto"));
+        let productName = prompt("Introduce el nombre del producto");
+        let productPrice = Number(prompt("Introduce el precio del producto"));
+        let productQuant = Number(prompt("Introduce la cantidad del producto"));
         var confirmation = prompt("Es correcto si o no?");
-        if ((productPrice * productQuant) > dinersInput) {
+        let total = productPrice * productQuant;
+        let restant = dinersInput - total;
+        //Todo el bloque anterior es de declaración de variables y sus respectivos parses.
+        if (total > dinersInput) { //Condicional para saber si se acepta la comanda o no dependiendo si se tiene más dinero de lo que cuesta.
+            console.log("ORDRE REBUTJADA");
+            console.log(`${productName} - ${productPrice}`);
+            console.log(`Quantitat demanada - ${productQuant}`);
+            console.log(`Total - ${total}`);
         }
         else {
+            console.log("ORDRE CONFIRMADA");
+            console.log(`${productName} - ${productPrice}`);
+            console.log(`Quantitat demanada - ${productQuant}`);
+            console.log(`Diners restants - ${restant}`);
         }
     }
-} while ((confirmation === null || confirmation === void 0 ? void 0 : confirmation.toLowerCase()) != "si");
+} while ((confirmation === null || confirmation === void 0 ? void 0 : confirmation.toLowerCase()) != "si"); //Bucle que te hace volver al inicio en caso de que te hayas equivocado al editar el producto o, ya no quieras añadir uno.
--- Practica2/src/ts/exercici1.ts
+++ Practica2/src/ts/exercici1.ts
@@ -8,21 +8,31 @@ do{
         break;
     }
     else{
-        var productName: string | null = prompt("Introduce el nombre del producto");
-        var productPrice: number = Number(prompt("Introduce el precio del producto"));
-        var productQuant: number= Number(prompt("Introduce la cantidad del producto"));
+
+        let productName: string | null = prompt("Introduce el nombre del producto");        
+        let productPrice: number = Number(prompt("Introduce el precio del producto"));
+        let productQuant: number= Number(prompt("Introduce la cantidad del producto"));
         var confirmation: string | null = prompt("Es correcto si o no?");
         
-        if((productPrice * productQuant) > dinersInput){
-    
+        let total: number = productPrice * productQuant;
+        let restant: number = dinersInput - total;
+        
+        //Todo el bloque anterior es de declaración de variables y sus respectivos parses.
+
+        if(total > dinersInput){   //Condicional para saber si se acepta la comanda o no dependiendo si se tiene más dinero de lo que cuesta.
+            console.log("ORDRE REBUTJADA");
+            console.log(`${productName} - ${productPrice}`);
+            console.log(`Quantitat demanada - ${productQuant}`);
+            console.log(`Total - ${total}`);
         }
         else{
-        
+            console.log("ORDRE CONFIRMADA");
+            console.log(`${productName} - ${productPrice}`);
+            console.log(`Quantitat demanada - ${productQuant}`);
+            console.log(`Diners restants - ${restant}`);
         }
         
     }
-
     
-    
-} while (confirmation?.toLowerCase() !="si") 
+} while (confirmation?.toLowerCase() !="si") //Bucle que te hace volver al inicio en caso de que te hayas equivocado al editar el producto o, ya no quieras añadir uno.
 
